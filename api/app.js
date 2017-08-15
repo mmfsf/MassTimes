c22c3813eeacf6722d080bd56132dfb302587056
Marcos Farias <mmfsf@cesar.org.br>
2017-08-15 11:59:45 -0300

    add server.js file. change app.js to export start method

--- api/app.js
+++ api/app.js
@@ -31,8 +31,11 @@ app.get('/', function(req, res) {
 	res.send('Mass Time');
 });
 
-var listener = app.listen(3000, function() {
-	console.log('Started!');
-	console.log(listener.address().port);
-	console.log(listener.address().address);
-});
+module.exports.start = function start(){
+	
+	var listener = app.listen(3000, function() {
+		console.log('Started!');
+		console.log(listener.address().port);
+		console.log(listener.address().address);
+	});
+}
---
+++ server.js
@@ -0,0 +1,7 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var app = require('./api/app');
+var server = app.start();
