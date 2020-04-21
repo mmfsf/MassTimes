57553edf6e9437e2ce6b11d57c08a5981c95d721
Marcos Miguel <marcosmiguel@outlook.com>
2020-04-21 10:00:22 +0200

    update angular core

---
+++ code/masstimes.api/AppExceptionHandler.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using System.IO;
+
+namespace masstimes.api
+{
+    public class AppExceptionHandler
+    {
+        public static void ExceptionHandler(IApplicationBuilder app)
+        {
+            app.UseExceptionHandler(errorApp =>
+            {
+                errorApp.Run(async context =>
+                {
+                    context.Response.StatusCode = 500;
+                    context.Response.ContentType = "text/html";
+
+                    await context.Response.WriteAsync("<html lang=\"en\"><body>\r\n");
+                    await context.Response.WriteAsync("ERROR!<br><br>\r\n");
+
+                    var exceptionHandlerPathFeature =
+                        context.Features.Get<IExceptionHandlerPathFeature>();
+
+                    var exceptionHandlerFeature = context.Features.Get<IExceptionHandlerFeature>();
+
+                    await context.Response.WriteAsync($"<p>{exceptionHandlerFeature?.Error}</p>");
+
+                    if (exceptionHandlerPathFeature?.Error is FileNotFoundException)
+                    {
+                        await context.Response.WriteAsync("File error thrown!<br><br>\r\n");
+                    }
+
+                    await context.Response.WriteAsync("<a href=\"/\">Home</a><br>\r\n");
+                    await context.Response.WriteAsync("</body></html>\r\n");
+                    await context.Response.WriteAsync(new string(' ', 512)); // IE padding
+                });
+            });
+        }
+    }
+}
