9154c2ecac11ba254f4f3ba1e4f2264ae7df7a1e
Marcos Miguel <marcosmiguel@outlook.com>
2020-05-03 15:24:08 +0200

    add thumb on mass time view add Thumb on models

--- code/masstimes.ui/ClientApp/src/app/components/masstime/masstime.component.ts
+++ code/masstimes.ui/ClientApp/src/app/components/masstime/masstime.component.ts
@@ -1,39 +1,40 @@
 import { Component, Input } from '@angular/core';
 
 @Component({
-    selector: 'app-masstime',
-    templateUrl: './masstime.component.html',
-    styleUrls: ['./masstime.component.scss']
+  selector: 'app-masstime',
+  templateUrl: './masstime.component.html',
+  styleUrls: ['./masstime.component.scss']
 })
 export class MassTimeComponent {
 
-    public massetimes: Array<any>;
+  public massetimes: Array<any>;
 
-    constructor() {
-        this.massetimes = new Array<any>();
+  constructor() {
+    this.massetimes = new Array<any>();
+  }
+
+  @Input()
+  set items(values: any) {
+    this.massetimes = [];
+    if (values === undefined) {
+      return;
     }
 
-    @Input()
-    set items(values: any) {
-        this.massetimes = [];
-        if (values === undefined) {
-            return;
-        }
-
-        values.map(c => {
-            if (!this.massetimes.some(x => x.church === c.church)) {
-                this.massetimes.push({
-                    church: c.church,
-                    city: c.city,
-                    neighborhood: c.neighborhood,
-                    weekday: c.weekday,
-                    address: c.address.replace(/, null/g, ''),
-                    time: new Array<any>()
-                });
-            } else {
-                const mass = this.massetimes.find(({ church }) => church === c.church);
-                mass.time.push({time: c.time, weekday: c.weekday});
-            }
+    values.map(c => {
+      if (!this.massetimes.some(x => x.church === c.church)) {
+        this.massetimes.push({
+          church: c.church,
+          city: c.city,
+          neighborhood: c.neighborhood,
+          weekday: c.weekday,
+          address: c.address.replace(/, null/g, ''),
+          time: new Array<any>(),
+          thumb: c.thumb ?? 'https://masstimesfiles.blob.core.windows.net/general/church.png'
         });
-    }
+      } else {
+        const mass = this.massetimes.find(({ church }) => church === c.church);
+        mass.time.push({ time: c.time, weekday: c.weekday });
+      }
+    });
+  }
 }
--- code/masstimes.ui/ClientApp/src/app/models/masstime.model.ts
+++ code/masstimes.ui/ClientApp/src/app/models/masstime.model.ts
@@ -1,22 +1,25 @@
 export class MassTime {
-    id: number;
-    time: any;
-    church: string;
-    weekday: string;
-    shortWeekDay: string;
-    city: string;
-    neighborhood: string;
-    address: string;
+  id: number;
+  time: any;
+  church: string;
+  weekday: string;
+  shortWeekDay: string;
+  city: string;
+  neighborhood: string;
+  address: string;
+  thumb: string;
 
-    constructor(id: number, time: any, church: string, weekday: string,
-                shortWeekDay: string, city: string, neighborhood: string, address: string) {
-        this.id = id;
-        this.time = time;
-        this.church = church;
-        this.weekday = weekday;
-        this.shortWeekDay = shortWeekDay;
-        this.city = city;
-        this.neighborhood = neighborhood;
-        this.address = address;
-    }
+  constructor(id: number, time: any, church: string, weekday: string,
+    shortWeekDay: string, city: string, neighborhood: string, address: string,
+    thumb: string) {
+    this.id = id;
+    this.time = time;
+    this.church = church;
+    this.weekday = weekday;
+    this.shortWeekDay = shortWeekDay;
+    this.city = city;
+    this.neighborhood = neighborhood;
+    this.address = address;
+    this.thumb = thumb;
+  }
 }
